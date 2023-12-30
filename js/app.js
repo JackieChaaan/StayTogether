58447b283b156837b13465f9f58a7865ffe31e49
JackieChaaan <abelcvarghese12097@gmail.com>
2023-12-30 10:43:50 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -23,3 +23,28 @@ document.getElementById('submit').addEventListener('click', function () {
         }
     }
 });
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    let uname = document.getElementById('uname').value;
+    let chief = "abel";
+
+    if (!uname) {
+        alert("Hey! 😀, You didn't enter your name...! 😅");
+        return;
+    } else if (uname.length <= 3) {
+        alert("Oops! 😬 Enter your Name please.🙂");
+        return;
+    } else {
+        let lowerCaseUname = uname.toLowerCase();
+        let lowerCaseChief = chief.toLowerCase();
+
+        if (lowerCaseUname === lowerCaseChief) {
+            alert("Hey..😃! That's My name😂");
+            return;
+        } else {
+            window.location.href = 'show.html?username=' + encodeURIComponent(uname);
+        }
+    }
+});
